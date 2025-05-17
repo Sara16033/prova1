e9b5705feba3e20472b5600f15583e93490d9859
Sara16033 <saramormandi@gmail.com>
2025-05-17 15:33:07 +0200

    Ignorato test fallito

--- math.test.js
+++ math.test.js
@@ -5,7 +5,7 @@ test('addizione corretta: 2 + 2 = 4', () => {
   expect(risultato).toBe(4);
 });
 
-test('addizione errata: 3 + 5 = 7', () => {
+test.skip('addizione errata: 3 + 5 = 7', () => {
   const risultato = add(3, 5);
   expect(risultato).toBe(7);
 });
